fd558cf5b97bc43e72e81e8fb3771a817b729da6
Joseph Paul Cohen <joseph@josephpcohen.com>
2018-12-07 11:58:10 -0500

    mods to model and plotting

--- models/simple_cnn2.py
+++ models/simple_cnn2.py
@@ -13,34 +13,55 @@ class CNN(nn.Module):
             ),
             nn.Dropout(p=0.2),
             nn.BatchNorm2d(128),
-            nn.ReLU(),
+            nn.ReLU())
+        
+        self.conv2 = nn.Sequential(
             nn.Conv2d(
                 in_channels=128,
-                out_channels=32,
+                out_channels=128,
                 kernel_size=3,
                 stride=2,
                 padding=0,
             ),
             nn.Dropout(p=0.2),
-            nn.BatchNorm2d(32),
-            nn.ReLU(),
+            nn.BatchNorm2d(128),
+            nn.ReLU())
+        
+        self.conv3 = nn.Sequential(
             nn.Conv2d(
-                in_channels=32,
-                out_channels=8,
+                in_channels=128,
+                out_channels=64,
                 kernel_size=6,
                 stride=2,
                 padding=0,
             ),
-            nn.ReLU(),
-            nn.MaxPool2d(20, stride=1)
-        )
+            nn.ReLU())
+        
+        self.pool = nn.MaxPool2d(20, stride=1)
+        
         # make the last shape dynamic
         test_out = self.conv1(test_sample.unsqueeze(0))
+        test_out = self.conv2(test_out)
+        test_out = self.conv3(test_out)
+        test_out = self.pool(test_out)
+        
         test_out = (test_out.view(test_out.size(0), -1))
-        self.out = nn.Linear(test_out.shape[1], 2)
+        
+        self.out = nn.Sequential(
+            nn.Linear(test_out.shape[1], 100),
+            nn.ReLU(),
+            nn.Linear(100, 2))
 
     def forward(self, x):
-        x = self.conv1(x)
+        res = {}
+        res['conv1'] = self.conv1(x)
+        res['conv2'] = self.conv2(res['conv1'])
+        res['conv3'] = self.conv3(res['conv2'])
+        res['pool'] = self.pool(res['conv3'])
+        x = res['pool']
         x = x.view(x.size(0), -1)
         output = self.out(x)
-        return output, x
+        return output, res
+    
+    
+    
