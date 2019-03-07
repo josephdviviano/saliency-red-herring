686e4e5b0c4d85bfb4995b52bfa1b419622ce747
Joseph Paul Cohen <joseph@josephpcohen.com>
2019-03-07 13:25:17 -0500

    add nsamples as arg

--- gradmask/main.py
+++ gradmask/main.py
@@ -11,12 +11,15 @@ def run():
 @click.option('--config', '-cgf', type=click.Path(exists=True, resolve_path=True), help='Configuration file.')
 @click.option('-seed', type=int, help='Seed for split and model')
 @click.option('-penalise_grad', type=str, help='penalise_grad')
+@click.option('-nsamples', type=int, help='nsamples')
 def train(config, seed, penalise_grad):
     cfg = configuration.load_config(config)
     if not seed is None:
         cfg["seed"] = seed
     if not penalise_grad is None:
         cfg["penalise_grad"] = penalise_grad
+    if not nsamples is None:
+        cfg["nsamples"] = nsamples
     training.train(cfg)
 
 @run.command()
