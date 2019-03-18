cb4867554af950a77f45c79fc527a438d609cbc5
Becks Simpson <beckaroos@hotmail.com>
2019-03-18 14:27:52 -0400

    fixed skopt bugs in main and lung_skopt config file

--- gradmask/main.py
+++ gradmask/main.py
@@ -35,10 +35,10 @@ def train(config, seed, penalise_grad, nsamples_train, penalise_grad_usemask, co
 @click.option('-seed', type=int, help='Seed for split and model')
 @click.option('-penalise_grad', type=str, help='penalise_grad')
 @click.option('-nsamples_train', type=int, help='nsamples_train')
-@click.option('--n-iter', type=int, default=10, help='Configuration file.')
-@click.option('--base-estimator', type=click.Choice(["GP", "RF", "ET", "GBRT"]), default="GP", help='Estimator.')
-@click.option('--n-initial-points', type=int, default=10, help='Number of evaluations of func with initialization points before approximating it with base_estimator.')
-@click.option('--train-function', type=str, default="train", help='Training function to optimize over.')
+@click.option('--n_iter', type=int, default=10, help='Configuration file.')
+@click.option('--base_estimator', type=click.Choice(["GP", "RF", "ET", "GBRT"]), default="GP", help='Estimator.')
+@click.option('--n_initial_points', type=int, default=10, help='Number of evaluations of func with initialization points before approximating it with base_estimator.')
+@click.option('--train_function', type=str, default="train", help='Training function to optimize over.')
 def train_skopt(config, seed, penalise_grad, nsamples_train, n_iter, base_estimator, n_initial_points, train_function):
     cfg = configuration.load_config(config)
     cfg["skopt"] = True
@@ -46,7 +46,7 @@ def train_skopt(config, seed, penalise_grad, nsamples_train, n_iter, base_estima
         cfg["seed"] = seed
     if not penalise_grad is None:
         cfg["penalise_grad"] = penalise_grad
-    if not nsamples is None:
+    if not nsamples_train is None:
         cfg["dataset"]["train"]["nsamples"] = nsamples_train
     training.train_skopt(cfg, n_iter=n_iter,
                     base_estimator=base_estimator,
