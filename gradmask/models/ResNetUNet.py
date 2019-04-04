91f334048b68349d8a49ef6478861affc137a1a4
Becks Simpson <beckaroos@hotmail.com>
2019-04-04 12:59:31 -0400

    updates

--- gradmask/models/ResNetUNet.py
+++ gradmask/models/ResNetUNet.py
@@ -13,29 +13,31 @@ def convrelu(in_channels, out_channels, kernel, padding):
 @register.setmodelname("ResNetUNet")
 class ResNetUNet(nn.Module):
 
-    def __init__(self, n_class, img_size=300):
+    def __init__(self, flat_layer=1, n_class=2, img_size=300):
         super().__init__()
+        
+        flat_layer = flat_layer * 200
 
         # Use ResNet18 as the encoder with the pretrained weights
         self.base_model = models.resnet18(pretrained=True)
-        self.base_layers = list(base_model.children())
+        self.base_layers = list(self.base_model.children())
 
         self.layer0 = nn.Sequential(*self.base_layers[:3]) # size=(N, 64, x.H/2, x.W/2)
-        self.layer0_1x1 = convrelu(64, 64, 1, 0)
+        self.layer0_1x1 = convrelu(32, 64, 1, 0)
         self.layer1 = nn.Sequential(*self.base_layers[3:5]) # size=(N, 256, x.H/4, x.W/4)
-        self.layer1_1x1 = convrelu(256, 256, 1, 0)
+        self.layer1_1x1 = convrelu(64, 128, 1, 0)
         self.layer2 = self.base_layers[5]  # size=(N, 512, x.H/8, x.W/8)
-        self.layer2_1x1 = convrelu(512, 512, 1, 0)
+        self.layer2_1x1 = convrelu(128, 256, 1, 0)
         self.layer3 = self.base_layers[6]  # size=(N, 1024, x.H/16, x.W/16)
-        self.layer3_1x1 = convrelu(1024, 512, 1, 0)
+        self.layer3_1x1 = convrelu(256, 512, 1, 0)
         self.layer4 = self.base_layers[7]  # size=(N, 2048, x.H/32, x.W/32)
-        self.layer4_1x1 = convrelu(2048, 1024, 1, 0)
+        self.layer4_1x1 = convrelu(512, 1024, 1, 0)
         
         self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
 
         self.conv_up3 = convrelu(512 + 1024, 512, 3, 1)
-        self.conv_up2 = convrelu(512 + 512, 512, 3, 1)
-        self.conv_up1 = convrelu(256 + 512, 256, 3, 1)
+        self.conv_up2 = convrelu(256 + 512, 512, 3, 1)
+        self.conv_up1 = convrelu(128 + 512, 256, 3, 1)
         self.conv_up0 = convrelu(64 + 256, 128, 3, 1)
 
         self.conv_original_size0 = convrelu(3, 64, 3, 1)
@@ -47,7 +49,7 @@ class ResNetUNet(nn.Module):
         
         #self.relu5 = nn.ReLU()
         
-        self.out = nn.Linear(128, num_class)
+        self.out = nn.Linear(128, n_class)
     
     def get_fc_layer_size(self, img_size):
         # self.conv_last = nn.Conv2d(64, n_class, 1)
@@ -71,6 +73,8 @@ class ResNetUNet(nn.Module):
         return(output)
         
     def forward(self, input_img):
+        # hack lol
+        input_img = torch.cat([input_img, input_img, input_img], dim=1)
         x_original = self.conv_original_size0(input_img)
         x_original = self.conv_original_size1(x_original)
 
@@ -85,6 +89,7 @@ class ResNetUNet(nn.Module):
         x = self.upsample(layer4)
         # Create the shortcut from the encoder
         layer3 = self.layer3_1x1(layer3)
+        # print("x shape: ", x.shape, "layer3 shape: ", layer3.shape)
         x = torch.cat([x, layer3], dim=1)
         x = self.conv_up3(x)
 
@@ -95,6 +100,7 @@ class ResNetUNet(nn.Module):
 
         x = self.upsample(x)
         layer1 = self.layer1_1x1(layer1)
+        print("x shape: ", x.shape, "layer1 shape: ", layer2.shape)
         x = torch.cat([x, layer1], dim=1)
         x = self.conv_up1(x)
 
@@ -106,7 +112,7 @@ class ResNetUNet(nn.Module):
         x = self.upsample(x)
         x = torch.cat([x, x_original], dim=1)
         x = self.conv_original_size2(x)
-
+        x = x.view(x.size(0), -1)
         out = self.fc1(x) # self.conv_last(x)
         out = self.out(out)
 
