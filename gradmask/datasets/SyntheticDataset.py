f1c7d8da2723aa317bfa43602bc07d4a13b43c68
Joseph Paul Cohen <joseph@josephpcohen.com>
2019-04-30 15:16:34 -0400

    add a distract_noise option to the synth dataloader

--- gradmask/datasets/SyntheticDataset.py
+++ gradmask/datasets/SyntheticDataset.py
@@ -5,15 +5,17 @@ import skimage, skimage.transform
 import numpy as np
 import utils.register as register
 from PIL import Image
+import torch
 import torchvision.transforms.functional as TF
 
 @register.setdatasetname("SyntheticDataset")
 class SyntheticDataset(Dataset):
-    def __init__(self, mode, dataroot, blur=0, seed=0, nsamples=32, maxmasks=32, transform=None, new_size=28):
+    def __init__(self, mode, dataroot, blur=0, seed=0, nsamples=32, maxmasks=32, transform=None, new_size=28, distract_noise=0):
 
         self.root = dataroot
         self.mode = mode
         self.blur = blur
+        self.distract_noise = distract_noise
         
         self._all_files = [f for f in os.listdir(self.root) if "seg" not in f and ".csv" not in f]
         self._seg_files = [f for f in os.listdir(self.root) if "seg" in f and ".csv" not in f]
@@ -22,8 +24,12 @@ class SyntheticDataset(Dataset):
         np.random.shuffle(self._all_files)
         
         # get the files for each mode
+        self.mode_file=mode
         if self.mode == "train":
             self.files = [f for f in self._all_files if "train" in f]
+        elif self.mode == "train_flip":
+            self.files = [f for f in self._all_files if "train" in f]
+            self.mode_file="train"
         elif self.mode == "valid":
             self.files = [f for f in self._all_files if "valid" in f]
         elif self.mode == "test":
@@ -32,7 +38,7 @@ class SyntheticDataset(Dataset):
             raise Exception("Unknown mode")
         
         # get the corresponding labels
-        all_labels = pd.read_csv("{}/{}_labels.csv".format(self.root, mode))
+        all_labels = pd.read_csv("{}/{}_labels.csv".format(self.root, self.mode_file))
         
         # randomly choose based on nsamples
         np.random.seed(seed)
@@ -67,6 +73,15 @@ class SyntheticDataset(Dataset):
         img = TF.to_tensor(img)
         seg = TF.to_tensor(seg)
         label = self.labels[index]
+        
+        if self.mode == "train_flip":
+            img = torch.flip(img,[2])
+            seg = torch.flip(seg,[2])
+            
+        if self.distract_noise != 0:
+            if np.random.choice([True,False], p=[self.distract_noise, 1-self.distract_noise]):
+                img = torch.flip(img,[2])
+                seg = torch.flip(seg,[2])
         
         # TODO: fix maxmasks so that the 1 returned here is whether the img mask was selected to be used
         return (img, seg), int(label), 1
