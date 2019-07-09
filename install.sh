c5b7bfebd6ffd5984ea2ab74c8dfb7f8c54ec340
Joseph Viviano <vivianoj@server.mila.quebec>
2019-07-09 15:58:27 -0400

    cleaned up install script

--- install.sh
+++ install.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-#Install script. setup the virtual environment and everything
 
-conda create -n gradmask python=3.5
-source activate gradmask # Need to run this everytime we want to run the project.
-sudo $(which pip) install -e .
-sudo $(which pip) install -r requirements.txt
-
-export LC_ALL=C.UTF-8 
+export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 
+conda create -n activmask python=3.5
+source activate activmask
+
+$(which pip) install -e .
+$(which pip) install -r requirements.txt
