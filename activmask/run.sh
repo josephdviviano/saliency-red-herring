3ae484722c535d88667d7087aa8dc8d77e02d5f2
Joseph Viviano <vivianoj@server.mila.quebec>
2019-07-16 14:05:57 -0400

    experiment table run

--- activmask/run.sh
+++ activmask/run.sh
@@ -13,12 +13,13 @@ python -u main.py train --config config/synth_ae_reconmasked.yml
 
 python -u main.py train --config config/synth_unet_activmask.yml
 python -u main.py train --config config/synth_unet_clf.yml
-python -u main.py train --config config/synth_ae_noactdiff.yml
+python -u main.py train --config config/synth_unet_noactdiff.yml
 python -u main.py train --config config/synth_unet_norecon.yml
-python -u main.py train --config config/synth_ae_reconmasked.yml
+python -u main.py train --config config/synth_unet_reconmasked.yml
 
 python -u main.py train --config config/synth_cnn_clf.yml
 python -u main.py train --config config/synth_cnn_norecon.yml
+
 python -u main.py train --config config/synth_resnet_clf.yml
 python -u main.py train --config config/synth_resnet_norecon.yml
 
