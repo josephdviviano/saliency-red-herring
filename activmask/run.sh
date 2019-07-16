f51a6b9cbc6e9d59bb85939ddea70a2805233fda
Joseph Viviano <vivianoj@server.mila.quebec>
2019-07-16 00:31:52 -0400

    added full synthetic experiment grid

--- activmask/run.sh
+++ activmask/run.sh
@@ -4,7 +4,27 @@ export LANG=C.UTF-8
 source $HOME/.bashrc
 source activate activmask
 
-python -u main.py train --config config/synth.yml $@
+# Main experiment grid!
+python -u main.py train --config config/synth_ae_activmask.yml #$@
+python -u main.py train --config config/synth_ae_clf.yml
+python -u main.py train --config config/synth_ae_noactdiff.yml
+python -u main.py train --config config/synth_ae_norecon.yml
+python -u main.py train --config config/synth_ae_reconmasked.yml
+
+python -u main.py train --config config/synth_unet_activmask.yml
+python -u main.py train --config config/synth_unet_clf.yml
+python -u main.py train --config config/synth_ae_noactdiff.yml
+python -u main.py train --config config/synth_unet_norecon.yml
+python -u main.py train --config config/synth_ae_reconmasked.yml
+
+python -u main.py train --config config/synth_cnn_clf.yml
+python -u main.py train --config config/synth_cnn_norecon.yml
+python -u main.py train --config config/synth_resnet_clf.yml
+python -u main.py train --config config/synth_resnet_norecon.yml
+
+# Control experiment: classification using masked training data.
+python -u main.py train --config config/synth_cnn_maskonly.yml
+python -u main.py train --config config/synth_resnet_maskonly.yml
 
 rc=$?
 echo "Exit code is $?"
