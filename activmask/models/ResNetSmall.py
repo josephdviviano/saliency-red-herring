05f9fdd1c7fb6434b3e9d3d91776f5c807a25fe7
Joseph Viviano <vivianoj@server.mila.quebec>
2019-07-16 14:05:10 -0400

    ResNetSmall model now supports activation differences.

--- activmask/models/ResNetSmall.py
+++ activmask/models/ResNetSmall.py
@@ -5,7 +5,7 @@ For Pre-activation ResNet, see 'preact_resnet.py'.
 Reference:
 [1] Kaiming He, Xiangyu Zhang, Shaoqing Ren, Jian Sun
     Deep Residual Learning for Image Recognition. arXiv:1512.03385
-    
+
 Code from https://github.com/kuangliu/pytorch-cifar/blob/master/models/resnet.py
 '''
 import torch
@@ -23,6 +23,8 @@ class BasicBlock(nn.Module):
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
 
+        self.activation = nn.ReLU()
+
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != self.expansion*planes:
             self.shortcut = nn.Sequential(
@@ -31,11 +33,18 @@ class BasicBlock(nn.Module):
             )
 
     def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
+        """Pass through x, saving all pre-activation states."""
+        all_activations = []
+
+        out = self.bn1(self.conv1(x))
+        all_activations.append(out)
+        out = self.activation(out)
+
         out = self.bn2(self.conv2(out))
         out += self.shortcut(x)
-        out = F.relu(out)
-        return out
+        all_activations.append(out)
+        out = self.activation(out)
+        return (out, all_activations)
 
 
 class Bottleneck(nn.Module):
@@ -50,6 +59,8 @@ class Bottleneck(nn.Module):
         self.conv3 = nn.Conv2d(planes, self.expansion*planes, kernel_size=1, bias=False)
         self.bn3 = nn.BatchNorm2d(self.expansion*planes)
 
+        self.activation = nn.ReLU()
+
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != self.expansion*planes:
             self.shortcut = nn.Sequential(
@@ -58,41 +69,73 @@ class Bottleneck(nn.Module):
             )
 
     def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = F.relu(self.bn2(self.conv2(out)))
+        all_activations = []
+
+        out = self.bn1(self.conv1(x))
+        all_activations.append(out)
+        out = nn.activation(out)
+
+        out = self.bn2(self.conv2(out))
+        all_activations.append(out)
+        out = nn.activation(out)
+
         out = self.bn3(self.conv3(out))
         out += self.shortcut(x)
-        out = F.relu(out)
-        return out
+        all_activations.append(out)
+        out = nn.activation(out)
+        return (out, all_activations)
 
 
 class ResNet(nn.Module):
     def __init__(self, block, num_blocks, num_classes=2):
         super(ResNet, self).__init__()
         self.in_planes = 64
+        self.all_activations = []
+        self.activation = nn.ReLU()
 
         self.conv1 = nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
-        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.layer1 = self._make_layers(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layers(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layers(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layers(block, 512, num_blocks[3], stride=2)
         self.linear = nn.Linear(512*block.expansion, num_classes)
 
-    def _make_layer(self, block, planes, num_blocks, stride):
+    def _make_layers(self, block, planes, num_blocks, stride):
         strides = [stride] + [1]*(num_blocks-1)
         layers = []
         for stride in strides:
             layers.append(block(self.in_planes, planes, stride))
             self.in_planes = planes * block.expansion
-        return nn.Sequential(*layers)
+        return nn.ModuleList(layers)
+
+    def _run_layers(self, layers, out):
+        all_activations = []
+        for layer in layers:
+            out, activations = layer(out)
+            all_activations.extend(activations)
+
+        return (out, all_activations)
 
     def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = self.layer1(out)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
+        self.all_activations = []  # Reset for all passes of network.
+
+        out = self.bn1(self.conv1(x))
+        self.all_activations.append(out)
+        out = self.activation(out)
+
+        out, activations = self._run_layers(self.layer1, out)
+        self.all_activations.extend(activations)
+
+        out, activations = self._run_layers(self.layer2, out)
+        self.all_activations.extend(activations)
+
+        out, activations = self._run_layers(self.layer3, out)
+        self.all_activations.extend(activations)
+
+        out, activations = self._run_layers(self.layer4, out)
+        self.all_activations.extend(activations)
+
         out = F.avg_pool2d(out, 4)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
@@ -119,17 +162,20 @@ def test():
     net = ResNet18()
     y = net(torch.randn(1,3,24,24))
     print(y.size())
-    
-    
+
+
 @register.setmodelname("ResNetSmall")
 class ResNetSmall(nn.Module):
 
     def __init__(self, **model_args):
         super(ResNetSmall, self).__init__()
         self.model = ResNet18()
+        self.all_activations = []
 
     def forward(self, x):
-        return self.model(x), None
-    
-    
-    
+        out = self.model(x)
+        self.all_activations = self.model.all_activations
+        return out, None
+
+
+
