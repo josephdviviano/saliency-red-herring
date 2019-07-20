8dbe19d35e68a3d15961835344b925760517953f
Joseph Viviano <vivianoj@server.mila.quebec>
2019-07-19 22:03:43 -0400

    added [01] scaling for the input images so thet play well with the reconstruction

--- activmask/datasets/MSDDataset.py
+++ activmask/datasets/MSDDataset.py
@@ -5,7 +5,7 @@ from skimage.io import imread, imsave
 from PIL import Image
 import skimage.filters
 import json
-import medpy, medpy.io 
+import medpy, medpy.io
 import numpy as np
 import collections
 import torchvision.transforms
@@ -19,28 +19,28 @@ def extract_samples(data, image_path, label_path):
     seg_data, _ = medpy.io.load(label_path)
     seg_data = seg_data.transpose(2,0,1)
     labels = seg_data.sum((1,2)) > 1
-    
+
     print (collections.Counter(labels))
-    
+
     for i in range(image_data.shape[0]):
         data.append((image_data[i],seg_data[i],labels[i]))
-        
+
 def extract_samples2(data, labels, image_path, label_path):
     image_data, _ = medpy.io.load(image_path)
     image_data = image_data.transpose(2,0,1)
     seg_data, _ = medpy.io.load(label_path)
     seg_data = seg_data.transpose(2,0,1)
     these_labels = seg_data.sum((1,2)) > 1
-    
+
     print (collections.Counter(these_labels))
-    
+
     for i in range(image_data.shape[0]):
         data.append([image_data[i],seg_data[i]])
         labels.append(these_labels[i])
 
-        
+
 def compute_hdf5(dataroot, files, hdf5_name):
-    
+
     with h5py.File(hdf5_name,"w") as hf:
         files = sorted(files, key=lambda k: k["image"])
         for i, p in enumerate(files):
@@ -63,6 +63,14 @@ def compute_hdf5(dataroot, files, hdf5_name):
             print(".")
             grp.create_dataset("labels",data=labels)
 
+def scale(image, maxval=1024):
+    """Assumes that maxvalue and minvalue are the same."""
+    image += maxval # minimum value is now 0
+    image /= maxval*2
+
+    return(image)
+
+
 #https://discuss.pytorch.org/t/torchvision-transfors-how-to-perform-identical-transform-on-both-image-and-target/10606
 def transform(image, mask, is_train, new_size):
 
@@ -71,7 +79,7 @@ def transform(image, mask, is_train, new_size):
             resize = torchvision.transforms.Resize(size=(new_size+10, new_size+10))
             image = resize(image)
             mask = resize(mask)
-            
+
             # Random crop
             i, j, h, w = torchvision.transforms.RandomCrop.get_params(
                 image, output_size=(new_size, new_size))
@@ -96,39 +104,43 @@ def transform(image, mask, is_train, new_size):
         # Transform to tensor
         image = TF.to_tensor(image)
         mask = TF.to_tensor(mask)
+
+        # Scale image to (maximum) ~[0 1]
+        image = scale(image)
         return image, mask
 
 
 cached_msd_ref = {}
-    
+
 class MSDDataset(Dataset):
 
-    def __init__(self, mode, dataroot, blur=0, seed=0, nsamples=32, maxmasks=32, transform=None, new_size=100):
-        
+    def __init__(self, mode, dataroot, blur=0, seed=0, nsamples=32,
+                 maxmasks=32, transform=None, new_size=100):
+
         self.mode = mode
         self.dataroot = dataroot
         self.new_size = new_size
-        
+
         filename = self.dataroot + "msd_gz_new.hdf5"
         if not os.path.isfile(filename):
             print("Computing hdf5 file of the data")
             dataset = json.load(open(self.dataroot + "dataset.json"))
             files = dataset['training']
             compute_hdf5(self.dataroot, files, filename)
-            
+
         #store cached reference so we can load the valid and test faster
         if not dataroot in cached_msd_ref:
             cached_msd_ref[dataroot] = h5py.File(filename,"r")
         self.dataset = cached_msd_ref[dataroot]
-        
+
         self._all_files = sorted(list(self.dataset.keys()))
-        
+
         all_labels = np.concatenate([self.dataset[i]["labels"] for i in self._all_files])
         print ("Full dataset contains: " + str(collections.Counter(all_labels)))
-        
+
         np.random.seed(seed)
         np.random.shuffle(self._all_files)
-        
+
         file_ratio = int(len(self._all_files)*0.3)
         print("mode=" + self.mode)
         if self.mode == "train":
@@ -139,7 +151,7 @@ class MSDDataset(Dataset):
             self.files = self._all_files[file_ratio*2:]
         else:
             raise Exception("Unknown mode")
-        
+
         print("Loading {} files:".format(len(self.files)) + str(self.files))
         #self.samples = np.concatenate([self.dataset[i]["slices"] for i in self.files])
         self.samples = []
@@ -149,24 +161,24 @@ class MSDDataset(Dataset):
         self.labels = np.concatenate([self.dataset[i]["labels"] for i in self.files])
         #self.transform = transform
         self.blur = blur
-        
+
         print ("Loaded images contain:" + str(collections.Counter(self.labels)))
-        
+
         self.idx = np.arange(self.labels.shape[0])
-        
+
         np.random.seed(seed)
         class0 = np.where(self.labels == 0)[0]
         class1 = np.where(self.labels == 1)[0]
         class0 = np.random.choice(class0, nsamples//2, replace=False)
         class1 = np.random.choice(class1, nsamples//2, replace=False)
         self.idx = np.append(class1, class0)
-        
+
         #these should be in order
         #self.samples = self.samples[self.idx]
         self.labels = self.labels[self.idx]
-        
+
         print ("This dataloader contains:" + str(collections.Counter(self.labels)))
-        
+
         # the samples start with labelled ones. past half there should be no labels
         self.mask_idx = self.idx[:maxmasks]
         # transform does nothing, it's pass in parameter only to make it compatible with everything else.
@@ -175,7 +187,7 @@ class MSDDataset(Dataset):
         return len(self.idx)
 
     def __getitem__(self, index):
-        
+
         key = self.samples[self.idx[index]]
         image,seg = self.dataset[key[0]]["slices"][str(key[1])]
         label = self.labels[index]
@@ -193,7 +205,7 @@ class MSDDataset(Dataset):
         seg = Image.fromarray(seg)
         #if self.transform != None:
             #seg = self.transform(seg)
-            
+
         if self.mode == "train":
             image, seg = transform(image, seg, True, self.new_size)
         else:
