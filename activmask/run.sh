976bf338e97f65104019695147ab95b35008d9fd
Joseph Viviano <vivianoj@server.mila.quebec>
2019-08-01 01:26:09 -0400

    new experiment grid with repeats and all liver experiments

--- activmask/run.sh
+++ activmask/run.sh
@@ -4,33 +4,41 @@ export LANG=C.UTF-8
 source $HOME/.bashrc
 source activate activmask
 
-# Main experiment grid!
-python -u main.py train --config config/synth_ae_actdiff+recon.yml
-python -u main.py train --config config/synth_ae_actdiff.yml
-python -u main.py train --config config/synth_ae_clf.yml
-python -u main.py train --config config/synth_ae_clfmasked.yml
-python -u main.py train --config config/synth_ae_recon.yml
-python -u main.py train --config config/synth_ae_reconmasked.yml
 
-python -u main.py train --config config/synth_cnn_actdiff.yml
-python -u main.py train --config config/synth_cnn_clf.yml
-python -u main.py train --config config/synth_cnn_clfmasked.yml
+for i in {1..10}; do
 
-python -u main.py train --config config/synth_resnet_actdiff.yml
-python -u main.py train --config config/synth_resnet_clf.yml
-python -u main.py train --config config/synth_resnet_clfmasked.yml
+    echo "ITERATION ${i}:"
 
-python -u main.py train --config config/synth_unet_actdiff+recon.yml
-python -u main.py train --config config/synth_unet_actdiff.yml
-python -u main.py train --config config/synth_unet_clf.yml
-python -u main.py train --config config/synth_unet_clfmasked.yml
-python -u main.py train --config config/synth_unet_recon.yml
-python -u main.py train --config config/synth_unet_reconmasked.yml
+    # Synthetic dataset experiments.
+    python -u main.py train --config config/synth_ae_actdiff+recon.yml
+    python -u main.py train --config config/synth_ae_actdiff.yml
+    python -u main.py train --config config/synth_ae_clf.yml
+    python -u main.py train --config config/synth_ae_clfmasked.yml
+    python -u main.py train --config config/synth_ae_recon.yml
+    python -u main.py train --config config/synth_ae_reconmasked.yml
+    python -u main.py train --config config/synth_cnn_actdiff.yml
+    python -u main.py train --config config/synth_cnn_clf.yml
+    python -u main.py train --config config/synth_cnn_clfmasked.yml
+    python -u main.py train --config config/synth_resnet_actdiff.yml
+    python -u main.py train --config config/synth_resnet_clf.yml
+    python -u main.py train --config config/synth_resnet_clfmasked.yml
+    python -u main.py train --config config/synth_unet_actdiff+recon.yml
+    python -u main.py train --config config/synth_unet_actdiff.yml
+    python -u main.py train --config config/synth_unet_clf.yml
+    python -u main.py train --config config/synth_unet_clfmasked.yml
+    python -u main.py train --config config/synth_unet_recon.yml
+    python -u main.py train --config config/synth_unet_reconmasked.yml
 
-rc=$?
-echo "Exit code is $?"
-if  [[ $rc == 0 ]]; then
-    echo "Command succeeded"
-else
-    echo "Command failed"
-fi
+    # Liver Experiments
+    python -u main.py train --config config/livermsd_ae_actdiff+recon.yml
+    python -u main.py train --config config/livermsd_ae_actdiff.yml
+    python -u main.py train --config config/livermsd_ae_clf.yml
+    python -u main.py train --config config/livermsd_cnn_actdiff.yml
+    python -u main.py train --config config/livermsd_cnn_clf.yml
+    python -u main.py train --config config/livermsd_resnet_actdiff.yml
+    python -u main.py train --config config/livermsd_resnet_clf.yml
+    python -u main.py train --config config/livermsd_unet_actdiff+recon.yml
+    python -u main.py train --config config/livermsd_unet_actdiff.yml
+    python -u main.py train --config config/livermsd_unet_clf.yml
+
+done
