91f2f2e23def05ea1dc3c9855790d07944b0d58b
Joseph Viviano <vivianoj@server.mila.quebec>
2019-08-02 18:33:51 -0400

    runs experiments on mila cluster fairly well

--- activmask/run_mila.sh
+++ activmask/run_mila.sh
@@ -1,17 +1,45 @@
 #!/bin/bash
-hostname
-export LANG=C.UTF-8
-source $HOME/.bashrc
-#source activate activmask
+
+mkdir cluster_logs
 
 EXPERIMENTS="synth livermsd"
+SEEDS=(1234 3232 3221 9856 1290 1987 3200 6400 8888 0451)
 
-for i in {1..10}; do
-    echo "ITERATION ${i}:"
+for seed in "${SEEDS[@]}"; do
+    echo "SEED ${seed}:"
+
+    if [ ${seed} -eq 1234 ]; then
+        viz="True"
+    else
+        viz="False"
+    fi
 
     for exp in ${EXPERIMENTS}; do
         for file in $(ls config/${exp}/${exp}_*); do
-            echo python -u main.py train --config ${file}
+
+            filename=$(basename ${file})
+            filename="${filename%.*}"
+            filename="${filename}_${seed}"
+            runscript="${filename}.pbs"
+            cat << EOF > ${runscript}
+#!/bin/bash
+#SBATCH --job-name=${filename}
+#SBATCH --output=cluster_logs/${filename}_out.txt
+#SBATCH --error=cluster_logs/${filename}_err.txt
+#SBATCH --ntasks=1
+#SBATCH --time=1:00:00
+#SBATCH --mem=8Gb
+#SBATCH --gres=gpu:titanxp:1
+
+hostname
+export LANG=C.UTF-8
+source $HOME/.bashrc
+source activate activmask
+python -u main.py train --config ${file} -seed=${seed} -viz=${viz}
+EOF
+
+        sbatch ${runscript}
+        rm ${runscript}
         done
     done
 done
