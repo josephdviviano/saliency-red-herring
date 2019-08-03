3fedc1c44f6ecb1af0f5d34912dd2a0840cdd378
Joseph Viviano <vivianoj@server.mila.quebec>
2019-08-03 11:12:52 -0400

    syncing configs home

--- activmask/run_mila.sh
+++ activmask/run_mila.sh
@@ -2,12 +2,14 @@
 
 mkdir cluster_logs
 
-EXPERIMENTS="synth livermsd"
+#EXPERIMENTS="synth livermsd"
+EXPERIMENTS="livermsd"
 SEEDS=(1234 3232 3221 9856 1290 1987 3200 6400 8888 0451)
 
 for seed in "${SEEDS[@]}"; do
     echo "SEED ${seed}:"
 
+    # Only generate visualizations for the first seed.
     if [ ${seed} -eq 1234 ]; then
         viz="True"
     else
@@ -21,13 +23,15 @@ for seed in "${SEEDS[@]}"; do
             filename="${filename%.*}"
             filename="${filename}_${seed}"
             runscript="${filename}.pbs"
+
+            # Generates a job script.
             cat << EOF > ${runscript}
 #!/bin/bash
 #SBATCH --job-name=${filename}
 #SBATCH --output=cluster_logs/${filename}_out.txt
 #SBATCH --error=cluster_logs/${filename}_err.txt
 #SBATCH --ntasks=1
-#SBATCH --time=1:00:00
+#SBATCH --time=3:00:00
 #SBATCH --mem=8Gb
 #SBATCH --gres=gpu:titanxp:1
 
@@ -38,7 +42,11 @@ source activate activmask
 python -u main.py train --config ${file} -seed=${seed} -viz=${viz}
 EOF
 
-        sbatch ${runscript}
+        # Only run jobs that don't already have an output log.
+        if [ ! -f cluster_logs/${filename}_out.txt ]; then
+            sbatch ${runscript}
+        fi
+
         rm ${runscript}
         done
     done
