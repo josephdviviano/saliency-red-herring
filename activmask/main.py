0867313d6617b3d7eb57aee70efa0e961dd9c7d7
Joseph Viviano <joseph@viviano.ca>
2020-07-08 13:32:55 +0000

    bugfix

--- activmask/main.py
+++ activmask/main.py
@@ -32,16 +32,13 @@ def train(config, seed):
 @click.option('--config', '-cgf',
               type=click.Path(exists=True, resolve_path=True),
               help='Configuration file.')
-@click.option('--n-iter',
-              type=int, default=10,
-              help='Configuration file.')
 @click.option('--base-estimator',
               type=click.Choice(["GP", "RF", "ET", "GBRT"]), default="GP",
               help='Estimator.')
 @click.option('--seed',
               type=int, default=None,
               help='Set random state to something other than None for reproducible results.')
-def train_skopt(config, base_estimator, n_initial_points, seed):
+def train_skopt(config, base_estimator, seed):
     cfg = configuration.load_config(config)
     training.train_skopt(cfg,
                          base_estimator=base_estimator,
