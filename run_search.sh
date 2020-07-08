20a12343486ebfac7472b7348b7c27a42a158a02
Joseph Viviano <joseph@viviano.ca>
2020-07-08 19:56:15 +0000

    removed seeds

--- run_search.sh
+++ run_search.sh
@@ -3,23 +3,18 @@
 mkdir cluster_logs
 
 EXPERIMENTS="synth-search" #livermsd-search cardiacmsd-search pancreasmsd-search xray-search"
-SEEDS=(1234)
-#SEEDS=(3232 3221 9856 1290 1987 3200 6400 8888 0451)
-N_ITER=30
+SEED=(1234)
 
-for seed in "${SEEDS[@]}"; do
-    echo "SEED ${seed}:"
+for exp in ${EXPERIMENTS}; do
+    for file in $(ls activmask/config/${exp}/${exp}_*); do
 
-    for exp in ${EXPERIMENTS}; do
-        for file in $(ls activmask/config/${exp}/${exp}_*); do
+        filename=$(basename ${file})
+        filename="${filename%.*}"
+        filename="${filename}_${seed}"
+        runscript="${filename}.pbs"
 
-            filename=$(basename ${file})
-            filename="${filename%.*}"
-            filename="${filename}_${seed}"
-            runscript="${filename}.pbs"
-
-            # Generates a job script.
-            cat << EOF > ${runscript}
+        # Generates a job script.
+        cat << EOF > ${runscript}
 #!/bin/bash
 #!/bin/bash
 #SBATCH --job-name=${filename}
@@ -35,12 +30,11 @@ for seed in "${SEEDS[@]}"; do
 hostname
 export LANG=C.UTF-8
 source $HOME/.bashrc
-python -u activmask/main.py train-skopt --config ${file} --seed=${seed} --n-iter=${N_ITER}
+python -u activmask/main.py train-skopt --config ${file} --seed=${SEED}
 EOF
 
-        # Only run jobs that don't already have an output log.
-        bash ${runscript}
-        rm ${runscript}
-        done
+    # Only run jobs that don't already have an output log.
+    bash ${runscript}
+    rm ${runscript}
     done
 done
